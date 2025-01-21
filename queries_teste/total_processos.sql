feb2e5fe274f51af42c7cba991e6dcd17bff7888
Itallo Santos <itallosantos@infox.com.br>
2025-01-21 08:57:51 -0300

    wip

--- queries_teste/total_processos.sql
+++ queries_teste/total_processos.sql
@@ -1,8 +1,1 @@
-SELECT
-    json_build_object(
-            'nr_processo', p.nr_processo,
-            'dt_distribuicao', tpt.dt_distribuicao
-    )
-FROM client.tb_processo_trf tpt
-JOIN core.tb_processo p on tpt.id_processo_trf = p.id_processo
-where p.nr_processo is not null and dt_distribuicao is not null limit 100;
+SELECT json_build_object('nr_processo', p.nr_processo, 'dt_distribuicao', tpt.dt_distribuicao) FROM client.tb_processo_trf tpt JOIN core.tb_processo p on tpt.id_processo_trf = p.id_processo where p.nr_processo is not null and dt_distribuicao is not null limit 100;
