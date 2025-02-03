f5d4e398df2fb24920301366925ec3331a8d08f9
Itallo Santos <itallosantos@infox.com.br>
2025-02-03 14:23:02 -0300

    wip

--- .github/scripts/updateConfig.js
+++ .github/scripts/updateConfig.js
@@ -5,15 +5,27 @@ const QUERIES_DIR = 'comum';
 const CONFIG_FILE = 'config.json';
 
 function getSQLFiles(dir) {
-  return fs.readdirSync(dir)
-    .filter(file => file.endsWith('.sql'))
-    .map(file => ({
-      nome: file.replace('.sql', '').replace(/_/g, ' '),
-      descricao: `Consulta SQL para ${file.replace('.sql', '').replace(/_/g, ' ')}.`,
-      caminho: path.join(QUERIES_DIR, file),
-      cron: "0 0 * * *",  // Default para execução diária à meia-noite
-      count_only: false   // Default como false
-    }));
+  let results = [];
+  const list = fs.readdirSync(dir);
+
+  list.forEach(file => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat && stat.isDirectory()) {
+      results = results.concat(getSQLFiles(filePath));
+    } else if (file.endsWith('.sql')) {
+      results.push({
+        nome: file.replace('.sql', '').replace(/_/g, ' '),
+        descricao: `Consulta SQL para ${file.replace('.sql', '').replace(/_/g, ' ')}.`,
+        caminho: filePath,
+        cron: "0 0 * * *",  // Default para execução diária à meia-noite
+        count_only: false   // Default como false
+      });
+    }
+  });
+
+  return results;
 }
 
 const queries = getSQLFiles(QUERIES_DIR);
